8f4a959093d18740e8b39126be0c5d9c7e0a7d21
Kenna-Tefera <annatefera3@gmail.com>
2022-05-13 23:49:49 +0300

    counting sort

---
+++ starting/counting_sort.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    n = len(arr)
+    freqarr = []
+    for i in range (0,100):
+        freqarr.append(0)
+    for i in range (0,n):
+        freqarr[arr[i]]+=1
+    return(freqarr)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
