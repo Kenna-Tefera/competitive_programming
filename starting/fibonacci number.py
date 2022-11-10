b32c728133fdf1826ec1bed3be16a6d31054ad01
Kenna-Tefera <annatefera3@gmail.com>
2022-11-10 13:46:42 +0300

    Time: 58 ms (56.590%), Space: 13.9 MB (9.51%) - LeetHub

--- starting/fibonacci number.py
+++ starting/fibonacci number.py
@@ -2,5 +2,9 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        else:
-            return self.fib(n - 1) + self.fib(n - 2)
+        bottom_up = [None] * (n+1)
+        bottom_up[0] = 0
+        bottom_up[1] = 1
+        for i in range(2, n+1):
+            bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+        return bottom_up[n]
